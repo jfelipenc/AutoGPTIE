a8f37453dd0e76ae3f5229e5452c9b167d2eab37
jfelipenc <jfelipenc99@gmail.com>
2023-10-02 14:47:57 -0300

    Added and commented section with soup parsing

--- autogpts/InsightEngineAgent/forge/sdk/abilities/file_system/web.py
+++ autogpts/InsightEngineAgent/forge/sdk/abilities/file_system/web.py
@@ -1,10 +1,11 @@
 import requests
+from bs4 import BeautifulSoup
 
 from ..registry import ability
 
 @ability(
     name="fetch_webpage",
-    description="Retrieves the content of a webpage as HTML code",
+    description="Retrieves the content of a webpage",
     parameters=[
         {
             "name": "url",
@@ -17,4 +18,6 @@ from ..registry import ability
 )
 async def fetch_webpage(agent, task_id: str, url: str) -> str:
     response = requests.get(url)
+    #soup = BeautifulSoup(response.content)
+    #return soup.get_text()
     return response.text
