98db3859d68bea6709d08a671013c854bb6b0286
jfelipenc <jfelipenc99@gmail.com>
2023-10-04 09:19:35 -0300

    Added logging and fetch_write_webpage ability

--- autogpts/InsightEngineAgent/forge/sdk/abilities/file_system/web.py
+++ autogpts/InsightEngineAgent/forge/sdk/abilities/file_system/web.py
@@ -1,7 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
+from forge.sdk import ForgeLogger
 from ..registry import ability
+from .files import write_file
+
+LOG = ForgeLogger(__name__)
 
 @ability(
     name="fetch_webpage",
@@ -18,6 +22,29 @@ from ..registry import ability
 )
 async def fetch_webpage(agent, task_id: str, url: str) -> str:
     response = requests.get(url)
-    #soup = BeautifulSoup(response.content)
-    #return soup.get_text()
     return response.text
+
+@ability(
+    name="fetch_write_webpage",
+    description="Retrieves the content of a webpage and saves to a .txt file",
+    parameters=[
+        {
+            "name": "url",
+            "description": "Webpage URL",
+            "type": "string",
+            "required": True
+        },
+        {
+            "name": "filepath",
+            "description": "Path to output TXT file",
+            "type": "string",
+            "required": True
+        }
+    ],
+    output_type="None"
+)
+async def fetch_write_webpage(agent, task_id: str, url: str, filepath: str):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content)
+    content = soup.get_text()
+    await write_file(agent, task_id, filepath, content)
